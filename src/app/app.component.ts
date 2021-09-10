c2b9356809ac25f0c0e30adfe8d1cdb90cbf87a2
Artem Tanyhin <magic.cat.775@gmail.com>
2021-09-09 23:15:19 -0400

    trying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 /*********************************************************************************
-* WEB422 – Assignment 05
+* WEB422 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students.
 *
-* Name: Artem Tanyhin    Student ID: 107958209     Date: 2021/03/28
+* Name: Artem Tanyhin    Student ID: 107958209     Date: 2021/04/11
+*
+* Online link: https://album-discoverer.netlify.app/
 *
 ********************************************************************************/
 
